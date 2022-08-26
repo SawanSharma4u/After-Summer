4a4238a9144b6c777a38ad767b8693e7ce60c1d8
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-08-26 16:03:03 +0530

    Time: 28 ms (82.67%), Space: 13.6 MB (87.69%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,25 +1,35 @@
 class Solution {
+    void makeset(vector<int>& parent, vector<int>& rank, int n){
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+    int findParent(int u, vector<int>& parent){
+        if(parent[u]==u){
+            return u;
+        }
+        return parent[u] = findParent(parent[u], parent);
+    }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<bool> visited(isConnected.size());
-        int cnt = 0;
-        for(int i = 0; i < isConnected.size(); i++){
-            if(!visited[i]){
-                cnt++;
-                queue<int> q;
-                q.push(i);
-                while(!q.empty()){
-                    int node = q.front();
-                    q.pop();
-                    visited[node] = true;
-                    for(int j = 0; j < isConnected.size(); j++){
-                        if(!visited[j] && isConnected[node][j]==1){
-                            q.push(j);
-                        }
+        int n = isConnected.size();
+        vector<int> parent(n);
+        vector<int> rank(n);
+        makeset(parent, rank, n);
+        int grp = n;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(isConnected[i][j]){
+                    int u = findParent(i, parent);
+                    int v = findParent(j, parent);
+                    if(u!=v){
+                        parent[u] = v;
+                        grp--;
                     }
                 }
             }
         }
-        return cnt;
+        return grp;
     }
 };
