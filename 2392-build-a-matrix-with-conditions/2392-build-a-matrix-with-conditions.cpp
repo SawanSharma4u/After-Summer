e058b2bcfc1aa2e3a8de7953bd2ef005a4acb070
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-08-30 19:23:18 +0530

    Time: 309 ms (18.05%), Space: 68.6 MB (8.57%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -33,18 +33,18 @@ public:
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
         //Find Topological sort or both mat;
         vector<int> row = find(rowConditions, k);
+        vector<vector<int>> res;
+        if(row.size()!=k) return res;
         vector<int> col = find(colConditions, k);
-        vector<vector<int>> ans;
-        if(row.size()!=k || col.size()!=k) return ans;
-        ans.resize(k, vector<int>(k));
-        for(int i = 0; i < k; i++){
-            for(int j = 0; j < k; j++){
-                if(row[i]==col[j]){
-                    ans[i][j] = row[i];
-                    break;
-                }
-            }
+        if(col.size()!=k) return res;
+        res.resize(k, vector<int>(k));
+        vector<int> idx(k+1,0);
+        for(int j=0;j<col.size();j++){
+            idx[col[j]] = j;
         }
-        return ans;
+        for(int i=0;i<k;i++){
+            res[i][idx[row[i]]] = row[i];
+        }
+        return res;
     }
 };
