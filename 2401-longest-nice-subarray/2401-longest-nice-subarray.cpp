24e34849582ebf8a6c28e9938181d77673ff328e
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-09-05 21:27:38 +0530

    Time: 220 ms (33.33%), Space: 57.1 MB (33.33%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,39 +1,13 @@
 class Solution {
-    int find(int i, int j, vector<int>& arr, int num){
-        while(i <= j){
-            int mid = (i+j)/2;
-            if((arr[i]&num)==0){
-                i = mid+1;
-            }
-            else{
-                j = mid-1;
-            }
-        }
-        return i;
-    }
-public:
+    public:
     int longestNiceSubarray(vector<int>& nums) {
-        int ans = 1;
-        int sum = nums[0];
-        int st = 0;
-        int cnt = 1;
-        vector<int> arr(nums.size());
-        arr[0] = nums[0];
-        for(int i = 1; i < nums.size(); i++){
-            if((sum&nums[i])==0){
-                sum = sum | nums[i];
-                arr[i] = sum;
-                cnt++;
-                ans = max(ans, cnt);
-            }
-            else{
-                st = find(st, i-1, arr, nums[i]);
-                cnt = 1;
-                i = ++st;
-                sum = nums[i];
-                arr[i] = nums[i];
-            }
+        int used = 0, j = 0, res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            while ((used & nums[i]) != 0)
+                used ^= nums[j++];
+            used |= nums[i];
+            res = max(res, i - j + 1);
         }
-        return ans;
+        return res;
     }
 };
