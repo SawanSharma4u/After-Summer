f082274a4123e789bb0d9f096971cb04192919cb
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-09-06 18:08:00 +0530

    Time: 19 ms (33.64%), Space: 9.5 MB (98.42%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int> > dp(grid.size(), vector<int>(grid[0].size()));
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(i==0){
-                    if(j==0) dp[i][j] = grid[i][j];
-                    else dp[i][j] = dp[i][j-1]+grid[i][j];
+    int minPathSum(vector<vector<int>>& dp) {
+        for(int i = 0; i < dp.size(); i++){
+            for(int j = 0; j < dp[0].size(); j++){
+                if(i==0 && j!=0){
+                    dp[i][j] += dp[i][j-1];
                 }
-                else{
-                    if(j==0) dp[i][j] = dp[i-1][j] + grid[i][j];
+                else if(i > 0){
+                    if(j==0) dp[i][j] += dp[i-1][j];
                     else{
-                        dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+                        dp[i][j] += min(dp[i-1][j], dp[i][j-1]);
                     }
                 }
             }
         }
-        return dp[grid.size()-1][grid[0].size()-1];
+        return dp[dp.size()-1][dp[0].size()-1];
     }
 };
