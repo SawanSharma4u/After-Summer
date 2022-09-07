5cc4a56d603c1f03e2b165a240dd688e0f097d7e
Sawan Kumar Sharma <sawansharmait@gmail.com>
2022-09-07 12:59:54 +0530

    Time: 75 ms (20.43%), Space: 7.7 MB (84.75%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(t.length() > s.length()) return "";
+         unordered_map<char, int> chek;
+        for(int i = 0; i < t.size(); i++){
+            ++chek[t[i]];
+        }
+        int i = -1, j = -1;
+        int k = 0, ans = INT_MAX;
+        int st, ed; 
+        bool f = true;
+        while(k < s.length()){
+            if(chek.find(s[k])!=chek.end()){
+                --chek[s[k]];
+                if(i==-1){
+                    i = k;
+                    j = k;
+                }
+                else{
+                    j = k;
+                }
+                bool flag = false;
+                for(auto i:chek){
+                    if(i.second > 0){
+                        flag = true;
+                    }
+                }
+                if(!flag){
+                    f = false;
+                    while(i < s.length() && chek.find(s[i])==chek.end() || chek[s[i]] < 0){
+                        if(chek.find(s[i])!=chek.end()){
+                            ++chek[s[i]];
+                        }
+                        i++;
+                    }
+                }
+                if(!flag && ans > j-i+1){
+                    st = i;
+                    ed = j;
+                    ans = j-i+1;
+                }
+            }
+            k++;
+        }
+        if(f) return "";
+        string str="";
+        for(; st <= ed; st++){
+            str += s[st];
+        }
+        return str;
+    }
+};
